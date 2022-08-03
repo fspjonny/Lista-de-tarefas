4cf7761ebcff0713790778529d78dc3f11bd54f7
Fábio Pedro <fabio.silvapedro76@gmail.com>
2022-08-03 12:11:33 -0300

    Change one Path on Core URL's

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('tarefas/admin/', admin.site.urls),
     path('', include('home.urls'), name='home'),
     path('todo/', include('todo.urls'), name='todo'),
     path('login/', include('authentication.urls'), name='login'),
